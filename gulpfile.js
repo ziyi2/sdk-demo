6089e46a2f891a33245463a0375f2a078efabda3
ziyi2 <18768107826@163.com>
2022-10-11 21:02:27 +0800

    feat: typescript 构建

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@ var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("default", function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
+  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("lib"));
 });
