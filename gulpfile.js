5c48b2b4faca4a4ac755762f5d1f7fcdc2013fa0
ziyi2 <18768107826@163.com>
2022-10-12 09:39:09 +0800

    feat: typescript 构建

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,6 @@ var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("default", function () {
+  // tsProject.src() 默认会基于 tsconfig.json 中的 files、exclude 和 include 指定的源文件进行编译
   return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("lib"));
 });
